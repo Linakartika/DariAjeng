65643b38453ff9a663a7a9a26edfa99e385833e5
Linakartika <linakartikasari97@gmail.com>
2017-04-05 09:42:35 +0700

    modifikasi lina

--- RPLMod8/src/com/company/Main.java
+++ RPLMod8/src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
             Scanner show = new Scanner(System.in);
+            System.out.print("ini update an dari LINA ");
             System.out.print("Masukan Jumlah Deret Fibonacci : ");
             int n = show.nextInt();
             long fib[] = new long[n];
